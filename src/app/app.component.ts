c8f5e6586db850646b44b656c666ed7071c9e552
Paolo <paolo.licup@catalisgov.com>
2023-09-15 05:41:21 +0800

    Editing the first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+    title = 'my-first-app';
+    name = 'pao';
 }
