7e4efeb97c4b65790747e9e92c74054f98febeb7
Paolo <paolo.licup@catalisgov.com>
2023-09-16 05:45:17 +0800

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AttributeSelectorComponent } from './server/attributeSelector.component';
 import { ServerComponent } from './server/server.component';
 import { ClassSelectorComponent } from './server/classSelector.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { ClassSelectorComponent } from './server/classSelector.component';
     ServerComponent,
     AttributeSelectorComponent,
     ClassSelectorComponent,
+    MyFormComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-my-form',
+    templateUrl: './my-form.component.html',
+    styleUrls: ['./my-form.component.css']
+})
+export class MyFormComponent implements OnInit {
+
+    username = "";
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
